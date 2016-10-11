9bacd1d8075ecc28a8dde6e921dea320fbdf10a5
Moncrieft <amoncrieft@gmail.com>
2016-10-10 20:39:29 -0400

    student class and things

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,35 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            student A = new student();
+            A.name = "ashley moncrieft";
+            A.hometown = "orangeburg, SC";
+            A.age = 30;
+            student B = new student();
+            B.name = "cecil sharp";
+            B.hometown = "somewhere else";
+            B.age = 31;
+
+            Console.WriteLine(A);
+
+            student C = new student();
+            Console.WriteLine("what's your name?");
+            C.name = Console.ReadLine();
+            Console.WriteLine("where are you from?");
+            C.hometown = Console.ReadLine();
+            Console.WriteLine("?cuantos anos tienes?");
+            C.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("here is your info\n" + C);
+            if (A.Equals(C))
+            { Console.WriteLine("pass"); }
+            else
+            {
+                Console.WriteLine("fail");
+            }
+
+            Console.ReadLine();
+         
         }
     }
 }
---
+++ ConsoleApplication1/ConsoleApplication1/student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class student
+    {
+        public string name;
+        public string hometown;
+        public int age;
+        public override bool Equals(object obj)
+        {
+            student other = obj as student;
+            if(other != null)
+            {
+                if(other.name == name && other.age == age && other.hometown == hometown)
+                { return true; }
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string info = "";
+            info += "Student Name: " + name;
+            info += "\nAge: " + age;
+            info += "\nHometown: " + hometown;
+            return info;
+        }
+    }
+}
