5ff802b8f541f4e3fb55e31f7bafe6e26bd7c612
Moncrieft <amoncrieft@gmail.com>
2016-10-17 18:32:37 -0400

    new exercise and made a class last time

---
+++ ConsoleApplication1/ConsoleApplication1/abstractclass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    abstract class vehicle
+    {
+        public string type;
+        public int maxriders;
+        public string fueltype;
+        public abstract void start();
+        public abstract void stop();
+    }
+
+    class car : vehicle
+    {
+        override public void start()
+            { Console.WriteLine("start"); }
+
+        override public void stop()
+            { Console.WriteLine("stop"); }
+
+    }
+    class chopper : vehicle
+    {
+
+    }
+    class bike : vehicle
+    {
+
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    i += i;
+                }
+                i++;
+            } while (i <= 1000);
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
